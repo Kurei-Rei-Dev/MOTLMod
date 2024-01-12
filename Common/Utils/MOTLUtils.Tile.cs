04a9f1fe94c685339795610757a604c8a1d96921
AnonZero960 <NullZero1@outlook.com>
2024-01-12 10:11:43 +0200

    Added TIle Merging

---
+++ Common/Utils/MOTLUtils.Tile.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+
+namespace MOTLMod.Common.Utils
+{
+    public partial class MOTLUtils
+    {
+        //To use this,,,
+        //In a tile's SetStaticDefaults....
+        //(Mod Solution name).tileMerge[Type, Mod.Find<ModTile>("{TILE_NAME}").Type] = true; (Without the curly brackets)
+        //MOTLMod.tileMerge[Type, Mod.Find<ModTile>("{TILE_NAME}").Type] = true;
+        //Repeat this if you want specific modded blocks to merge together.
+        // - Zero
+        public static void Merge(int tile, int tile2)
+        {
+            Main.tileMerge[tile][tile2] = true;
+            Main.tileMerge[tile2][tile] = true;
+        }
+    }
+}
--- MOTLMod.cs
+++ MOTLMod.cs
@@ -1,8 +1,25 @@
+using Terraria;
 using Terraria.ModLoader;
 
 namespace MOTLMod
 {
 	public class MOTLMod : Mod
 	{
-	}
+        //click the lil thing on the left side to expand and collapse the region :heart:
+        #region Tile Merge Stuff - Zero
+        //Tile Merge Stuff
+        // - Zero
+        private static TileTest v = new();
+        public static TileTest tileMerge => v;
+
+        public class TileTest
+        {
+            public bool this[int tile1, int tile2]
+            {
+                get => Main.tileMerge[tile1][tile2] || Main.tileMerge[tile2][tile1];
+                set => MOTLUtils.Merge(tile1, tile2);
+            }
+        }
+        #endregion
+    }
 }
