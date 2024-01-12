ac0389ab6be8247de63ef4ad4b8ceec12c1c924b
AnonZero960 <NullZero1@outlook.com>
2024-01-12 10:40:48 +0200

    Lmao i forgot to add pngs

---
+++ Content/Items/Placeables/Blocks/RuinItemSet/RuinPillar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MOTLMod.Content.Items.Placeables.Blocks.RuinItemSet
+{
+    public class RuinPillar : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 100;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 14;
+            Item.height = 14;
+            Item.maxStack = 9999;
+            Item.useTurn = true;
+            Item.autoReuse = true;
+            Item.useAnimation = 15;
+            Item.useTime = 10;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.consumable = true;
+            Item.createTile = Mod.Find<ModTile>("RuinPillar").Type;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(5).AddIngredient(ItemID.StoneBlock, 5).AddIngredient(ModContent.ItemType<RuinItemSet.RuinBrick>(), 5).AddTile(TileID.Furnaces).Register();
+        }
+    }
+}
--- Content/Tiles/LabyrinthSet/RuinPillar.cs
+++ Content/Tiles/LabyrinthSet/RuinPillar.cs
@@ -5,18 +5,18 @@ using Terraria.ModLoader;
 
 namespace MOTLMod.Content.Tiles.LabyrinthSet
 {
-    public class RuinBrick : ModTile
+    public class RuinPillar : ModTile
     {
         public override void SetStaticDefaults()
         {
             Main.tileSolid[Type] = true;
             Main.tileMergeDirt[Type] = true;
-            MOTLMod.tileMerge[Type,TileID.Stone] = true;
+            MOTLMod.tileMerge[Type, TileID.Stone] = true;
             MOTLMod.tileMerge[Type, TileID.ClayBlock] = true;
-            MOTLMod.tileMerge[Type, Mod.Find<ModTile>("RuinPillar").Type] = true;
+            MOTLMod.tileMerge[Type, Mod.Find<ModTile>("RuinBrick").Type] = true;
             Main.tileBlockLight[Type] = true;
             Main.tileLighted[Type] = false;
-            AddMapEntry(new Color(126, 85, 88));
+            AddMapEntry(new Color(78, 45, 56));
         }
     }
 }
