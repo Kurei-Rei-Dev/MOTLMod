228d99b4ed9aec3697c0572045c70ec8056e9ac8
AnonZero960 <NullZero1@outlook.com>
2024-02-14 21:24:05 +0200

    Downed boss, sound sys

---
+++ Common/Systems/DownedBossSys.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace MOTLMod.Common.Systems
+{
+    // Acts as a container for "downed boss" flags.
+    // Set a flag like this in your bosses OnKill hook:
+    //    NPC.SetEventFlagCleared(ref DownedBossSystem.downedMinionBoss, -1);
+
+    public class DownedBossSys : ModSystem
+    {
+        public static bool downedShadowtaur = false;
+        // public static bool blahblahblah = false;
+
+        public override void ClearWorld()
+        {
+            downedShadowtaur = false;
+            // blahblahblah = false;
+        }
+
+        // We save our data sets using TagCompounds.
+        // NOTE: The tag instance provided here is always empty by default.
+        public override void SaveWorldData(TagCompound tag)
+        {
+            if (downedShadowtaur)
+            {
+                tag["downedShadowtaur"] = true;
+            }
+
+            // if (downedother) {
+            //	tag["downedother"] = true;
+            // }
+        }
+
+        public override void LoadWorldData(TagCompound tag)
+        {
+            downedShadowtaur = tag.ContainsKey("downedShadowtaur");
+            //dwonedother = tag.ContainsKey("downedother");
+        }
+
+        public override void NetSend(BinaryWriter writer)
+        {
+            // Order of operations is important and has to match that of NetReceive
+            var flags = new BitsByte();
+            flags[0] = downedShadowtaur;
+            // flags[1] = downedother;
+            writer.Write(flags);
+        }
+
+        public override void NetReceive(BinaryReader reader)
+        {
+            // Order of operations is important and has to match that of NetSend
+            BitsByte flags = reader.ReadByte();
+            downedShadowtaur = flags[0];
+            // downedOtherBoss = flags[1];
+
+            // As mentioned in NetSend, BitBytes can contain up to 8 values. If you have more, be sure to read the additional data:
+        }
+    }
+}
---
+++ Common/Systems/MythSoundSys.cs
@@ -0,0 +1,19 @@
+﻿using MOTLMod.Content.NPCs.Bosses.PreHM.Minotaur;
+using Terraria.Audio;
+using Terraria.ModLoader;
+
+namespace MOTLMod.Common.Systems
+{
+
+    public class MythSoundSys : ModSystem
+    {
+       //.ogg files please
+        public static readonly SoundStyle ShadowtaurDeath;
+
+
+        static MythSoundSys()
+        {
+            ShadowtaurDeath = new SoundStyle("MOTLMod/Assets/Sounds/ShadowtaurDeath", (SoundType)0);
+        }
+    }
+}
